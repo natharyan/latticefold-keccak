164b33de692eb16aecf75afb853dd87ddda0bdeb
Alex Klein <matthewklein345@gmail.com>
2024-06-06 21:16:18 +0100

    Add comments and todo

--- ccs/src/r1cs.rs
+++ ccs/src/r1cs.rs
@@ -12,10 +12,11 @@ pub struct R1CS<R: Ring> {
 }
 
 impl<R: Ring> R1CS<R> {
+    // returns a tuple containing (w, x) (witness and public inputs respectively)
     pub fn split_z(&self, z: &[R]) -> (Vec<R>, Vec<R>) {
         (z[self.l + 1..].to_vec(), z[1..self.l + 1].to_vec())
     }
-
+    // check that a R1CS structure is satisfied by a z vector. Only for testing.
     pub fn check_relation(&self, z: &[R]) -> Result<(), NotSatisfiedError> {
         let Az = utils::mat_by_vec(&self.A, &z.to_vec());
         let Bz = utils::mat_by_vec(&self.B, &z.to_vec());
@@ -29,7 +30,8 @@ impl<R: Ring> R1CS<R> {
             Ok(())
         }
     }
-
+    // converts the R1CS instance into a RelaxedR1CS as described in
+    // [Nova](https://eprint.iacr.org/2021/370.pdf) section 4.1.
     pub fn relax(self) -> RelaxedR1CS<R> {
         RelaxedR1CS::<R> {
             l: self.l,
@@ -52,6 +54,7 @@ pub struct RelaxedR1CS<R: Ring> {
 }
 
 impl<R: Ring> RelaxedR1CS<R> {
+    /// check that a RelaxedR1CS structure is satisfied by a z vector.
     pub fn check_relation(&self, z: &[R]) -> Result<(), NotSatisfiedError> {
         let Az = mat_by_vec(&self.A, &z.to_vec());
         let Bz = mat_by_vec(&self.B, &z.to_vec());
@@ -67,3 +70,5 @@ impl<R: Ring> RelaxedR1CS<R> {
         }
     }
 }
+
+// TODO Find an example to test on

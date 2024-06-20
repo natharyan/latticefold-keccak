f9638acfde960a59af07ba397102e02caf2788c2
Alex Klein <matthewklein345@gmail.com>
2024-06-20 11:58:44 +0200

    Add Cyclotomic ring trait
    - Cyclotomic ring trait defines challenge set and to ntt
    - Add one implementation with the pbb prime

--- cyclotomic-rings/src/rings/mod.rs
+++ cyclotomic-rings/src/rings/mod.rs
@@ -1,4 +1,12 @@
+use lattirust_arithmetic::ring::Zq;
+use num_bigint::BigInt;
+
 mod pbb;
 mod pstark;
 mod pgold;
 mod pm31;
+
+pub trait CyclotomicRing<const Q: u64> {
+    fn get_challenge_set(&self) -> BigInt;
+    fn to_ntt(&self) -> Vec<Zq<Q>>;
+}
--- cyclotomic-rings/src/rings/pbb.rs
+++ cyclotomic-rings/src/rings/pbb.rs
@@ -1,1 +1,27 @@
 // pBB = 15 · 2^27 + 1
+
+use lattirust_arithmetic::{
+    ntt::NTT,
+    ring::{ Zq, Pow2CyclotomicPolyRingNTT, Pow2CyclotomicPolyRing },
+};
+use super::CyclotomicRing;
+use num_bigint::BigInt;
+
+const Q: u64 = 15 * (1 << 27) + 1;
+
+type ZqQ = Zq<Q>;
+
+pub struct PBBCyclotomicRing<const N: usize>(Pow2CyclotomicPolyRing<ZqQ, N>);
+
+impl<const N: usize> CyclotomicRing<Q> for PBBCyclotomicRing<N> {
+    // Returns integer modulus of the set
+    // TODO coverth this to the csmall set in the latticefold params document
+    fn get_challenge_set(&self) -> BigInt {
+        Q.into()
+    }
+
+    fn to_ntt(&self) -> Vec<ZqQ> {
+        let ntt_ring = Pow2CyclotomicPolyRingNTT::from(self.0);
+        ntt_ring.ntt_coeffs()
+    }
+}

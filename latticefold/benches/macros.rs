0dac34c518e6bc4cb5b7a2427626e5f5b4e12a19
Matthew Klein <matthewklein345@gmail.com>
2024-12-06 12:39:59 +0000

    Fix bench params (#140)
    Some powers of L were too low such that B^L<Q

--- latticefold/benches/macros.rs
+++ latticefold/benches/macros.rs
@@ -19,23 +19,23 @@ macro_rules! define_params {
 macro_rules! run_goldilocks_benchmarks {
     ($group: ident) => {
         // Parameters Criterion, X_LEN, C, W, B, L, B_small, K
-        run_single_goldilocks_benchmark!(&mut $group, 1, 6, 512, 128, 9, 2, 7);
+        run_single_goldilocks_benchmark!(&mut $group, 1, 6, 512, 128, 10, 2, 7);
         run_single_goldilocks_benchmark!(&mut $group, 1, 7, 512, 256, 8, 2, 8);
-        run_single_goldilocks_benchmark!(&mut $group, 1, 8, 512, 512, 7, 2, 9);
-        run_single_goldilocks_benchmark!(&mut $group, 1, 8, 1024, 512, 7, 2, 9);
+        run_single_goldilocks_benchmark!(&mut $group, 1, 8, 512, 512, 8, 2, 9);
+        run_single_goldilocks_benchmark!(&mut $group, 1, 8, 1024, 512, 8, 2, 9);
         run_single_goldilocks_benchmark!(&mut $group, 1, 8, 2048, 256, 8, 2, 8);
         run_single_goldilocks_benchmark!(&mut $group, 1, 9, 1024, 1024, 7, 2, 10);
-        run_single_goldilocks_benchmark!(&mut $group, 1, 9, 2048, 512, 7, 2, 9);
+        run_single_goldilocks_benchmark!(&mut $group, 1, 9, 2048, 512, 8, 2, 9);
         run_single_goldilocks_benchmark!(&mut $group, 1, 10, 512, 2048, 6, 2, 11);
         run_single_goldilocks_benchmark!(&mut $group, 1, 10, 1024, 2048, 6, 2, 11);
         run_single_goldilocks_benchmark!(&mut $group, 1, 11, 1024, 4096, 6, 2, 12);
-        run_single_goldilocks_benchmark!(&mut $group, 1, 11, 2048, 2048, 6, 2, 12);
-        run_single_goldilocks_benchmark!(&mut $group, 1, 12, 1024, 8192, 6, 2, 13);
+        run_single_goldilocks_benchmark!(&mut $group, 1, 11, 2048, 2048, 6, 2, 11);
+        run_single_goldilocks_benchmark!(&mut $group, 1, 12, 1024, 8192, 5, 2, 13);
         run_single_goldilocks_benchmark!(&mut $group, 1, 13, 1024, 16384, 5, 2, 14);
         run_single_goldilocks_benchmark!(&mut $group, 1, 13, 2048, 8192, 5, 2, 13);
         run_single_goldilocks_benchmark!(&mut $group, 1, 14, 1024, 32768, 5, 2, 15);
         run_single_goldilocks_benchmark!(&mut $group, 1, 14, 2048, 16384, 5, 2, 14);
-        run_single_goldilocks_benchmark!(&mut $group, 1, 15, 2048, 32768, 4, 2, 15);
+        run_single_goldilocks_benchmark!(&mut $group, 1, 15, 2048, 32768, 5, 2, 15);
         run_single_goldilocks_benchmark!(&mut $group, 1, 16, 2048, 65536, 4, 2, 16);
     };
 }
@@ -44,23 +44,23 @@ macro_rules! run_goldilocks_benchmarks {
 macro_rules! run_goldilocks_non_scalar_benchmarks {
     ($group: ident) => {
         // Parameters Criterion, X_LEN, C, W, B, L, B_small, K
-        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 6, 512, 128, 9, 2, 7);
+        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 6, 512, 128, 10, 2, 7);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 7, 512, 256, 8, 2, 8);
-        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 8, 512, 512, 7, 2, 9);
-        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 8, 1024, 512, 7, 2, 9);
+        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 8, 512, 512, 8, 2, 9);
+        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 8, 1024, 512, 8, 2, 9);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 8, 2048, 256, 8, 2, 8);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 9, 1024, 1024, 7, 2, 10);
-        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 9, 2048, 512, 7, 2, 9);
+        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 9, 2048, 512, 8, 2, 9);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 10, 512, 2048, 6, 2, 11);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 10, 1024, 2048, 6, 2, 11);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 11, 1024, 4096, 6, 2, 12);
-        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 11, 2048, 2048, 6, 2, 12);
-        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 12, 1024, 8192, 6, 2, 13);
+        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 11, 2048, 2048, 6, 2, 11);
+        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 12, 1024, 8192, 5, 2, 13);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 13, 1024, 16384, 5, 2, 14);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 13, 2048, 8192, 5, 2, 13);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 14, 1024, 32768, 5, 2, 15);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 14, 2048, 16384, 5, 2, 14);
-        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 15, 2048, 32768, 4, 2, 15);
+        run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 15, 2048, 32768, 5, 2, 15);
         run_single_goldilocks_non_scalar_benchmark!(&mut $group, 1, 16, 2048, 65536, 4, 2, 16);
     };
 }
@@ -69,11 +69,11 @@ macro_rules! run_goldilocks_non_scalar_benchmarks {
 macro_rules! run_starkprime_benchmarks {
     ($group: ident) => {
         // Parameters Criterion, X_LEN, C, W, B, L, B_small, K
-        run_single_starkprime_benchmark!(&mut $group, 1, 15, 1024, 3052596316u128, 1, 2, 30);
-        run_single_starkprime_benchmark!(&mut $group, 1, 16, 1024, 4294967296u128, 1, 2, 32);
-        run_single_starkprime_benchmark!(&mut $group, 1, 17, 2048, 8589934592u128, 1, 2, 33);
-        run_single_starkprime_benchmark!(&mut $group, 1, 18, 2048, 20833367754u128, 1, 2, 34);
-        run_single_starkprime_benchmark!(&mut $group, 1, 19, 2048, 34359738368u128, 1, 2, 35);
+        run_single_starkprime_benchmark!(&mut $group, 1, 15, 1024, 1073741824u128, 9, 2, 30);
+        run_single_starkprime_benchmark!(&mut $group, 1, 16, 1024, 4294967296u128, 8, 2, 32);
+        run_single_starkprime_benchmark!(&mut $group, 1, 17, 2048, 8589934592u128, 8, 2, 33);
+        run_single_starkprime_benchmark!(&mut $group, 1, 18, 2048, 17179869184u128, 8, 2, 34);
+        run_single_starkprime_benchmark!(&mut $group, 1, 19, 2048, 34359738368u128, 8, 2, 35);
     };
 }
 
@@ -86,8 +86,8 @@ macro_rules! run_starkprime_non_scalar_benchmarks {
             1,
             15,
             1024,
-            3052596316u128,
-            1,
+            1073741824u128,
+            9,
             2,
             30
         );
@@ -97,7 +97,7 @@ macro_rules! run_starkprime_non_scalar_benchmarks {
             16,
             1024,
             4294967296u128,
-            1,
+            8,
             2,
             32
         );
@@ -107,7 +107,7 @@ macro_rules! run_starkprime_non_scalar_benchmarks {
             17,
             2048,
             8589934592u128,
-            1,
+            8,
             2,
             33
         );
@@ -116,8 +116,8 @@ macro_rules! run_starkprime_non_scalar_benchmarks {
             1,
             18,
             2048,
-            20833367754u128,
-            1,
+            17179869184u128,
+            8,
             2,
             34
         );
@@ -127,7 +127,7 @@ macro_rules! run_starkprime_non_scalar_benchmarks {
             19,
             2048,
             34359738368u128,
-            1,
+            8,
             2,
             35
         );
